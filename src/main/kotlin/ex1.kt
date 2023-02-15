079e7d7ce5674c4a1b82e244c45dc4a40fd1a667
Khanh <lnkhanh.20it2@vku.udn.vn>
2023-02-15 18:44:35 +0700

    Coroutines basics

--- src/main/kotlin/ex1.kt
+++ src/main/kotlin/ex1.kt
@@ -1,13 +1,11 @@
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
+
 fun main() = runBlocking {
     launch {
-        doWorld()
+        delay(1000L)
+        println("World!")
     }
     println("Hello")
-}
-suspend fun doWorld() {
-    delay(1000L)
-    println("World!")
 }
