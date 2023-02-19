e117cd63388a63822913e92e455b8729384ba200
Khanh <lnkhanh.20it2@vku.udn.vn>
2023-02-19 18:19:27 +0700

    Coroutines exception handling

---
+++ src/main/kotlin/CoroutineExceptionHandler.kt
@@ -0,0 +1,14 @@
+import kotlinx.coroutines.*
+
+fun main() = runBlocking<Unit> {
+    val handler = CoroutineExceptionHandler { _, exception ->
+        println("CoroutineExceptionHandler got $exception")
+    }
+    val job = GlobalScope.launch(handler) { // root coroutine, running in GlobalScope
+        throw AssertionError()
+    }
+    val deferred = GlobalScope.async(handler) { // also root, but async instead of launch
+        throw ArithmeticException() // Nothing will be printed, relying on user to call deferred.await()
+    }
+    joinAll(job, deferred)
+}
